582d2bf310416f31a50111e92edf047a601939e8
benjamin_fan <benjamin_fan@trendmicro.com>
2022-12-30 18:24:50 +0800

    feat: add static and data file
    add background image
    add award and member list data
    add basic script for data access

---
+++ src/config/awards.js
@@ -0,0 +1,17 @@
+'use strict';
+const awards = [{
+   "title":"Fifth Prize",
+   "quota": 10
+},{
+    "title":"Fourth Prize",
+    "quota": 7
+},{
+    "title":"Third Prize",
+    "quota": 5
+},{
+    "title":"Second Prize",
+    "quota": 3
+},{
+    "title":"JACKPOT",
+    "quota": 1
+}];
